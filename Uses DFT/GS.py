44348f6268e5af7565be6bc05ab5c75fb811b562
frankelm <maxnfrankel@gmail.com>
2022-11-15 13:15:21 +0100

    Completed GS algorithm

--- Uses DFT/GS.py
+++ Uses DFT/GS.py
@@ -4,7 +4,24 @@ import scipy.fft as fft
 from PtArrayModule import PtArrayCoords
 from analyzeSimModule import perf
 from SRModule import SR
-import cmath
+
+def GS(slm_phase):
+    # get field at SLM
+    slm = np.exp(1j*slm_phase)
+
+    # get signal field and extract phase
+    signal = fft.fft2(slm)
+    signal_phase = np.angle(signal)
+
+    # set trap phase equal to that found in signal_phase
+    Vm_phase = np.multiply(trap_plane,np.exp(1j*signal_phase))
+
+    # create slm field and extract phase
+    slm = fft.ifft2(Vm_phase)
+    slm_phase = np.angle(slm)
+
+    # only return phase
+    return slm_phase
 
 # SLM array dimensions in pixels
 Nx = 768
@@ -31,40 +48,20 @@ plt.show()
 slm_phase = SR(Nx,Ny,xm,ym)
 
 # now it's time to do the GS algorithm
+print('Beginning GS Algorithm')
+niter = 30 # define a number of iterations to apply the GS algorithm
 
-# create 3D matrix for indexing delta_j^m, where j has two coords xj,yj, and m is 1D
-# the ordering of yj,m,xj is important so that np.multiply(xj,xm[m]) and np.multiply(yj,ym) are multiplying the correct temrs
-yj,m,xj = np.meshgrid(range(Ny),range(xm.size),range(Nx))
+for i in range(niter):
+    slm_phase = GS(slm_phase)
+    print(i)
 
-# calculate delta_j^m
-# xj,yj are 3D arrays where only the x,y coord is indexed, respectively
-# m is a 3D array where each 2D slice in z corresponds to a different trap number
-# thus, xm[m] is a 3D array where each slice in z is all the same value, x[m]. ym[m] is similar
-# this is just used to speed up calculation so we don't need to do a for loop over every xj and yj to repeat calculation
-xjxm = np.multiply(xj,xm[m])/Nx # xm is actually supposed to be a frequency coordinate xm/Nx that we get out in the fourier transform
-yjym = np.multiply(yj,ym[m])/Ny
-
-print("Beginning Gershberg-Saxton algorithm")
-
-delta_j_m = 2.0*cmath.pi*np.add( xjxm, yjym )
-
-# calculate Vm/|Vm|
-# Vm = sum over j of 1/N*exp(1j*(phi_j-delta_j_m)), should be a 1d array of length M
-# What we really want is the phase of Vm. We don't need its length since we're dividing by |Vm|
-Vm_phase = np.angle(np.sum(np.sum(np.exp(1j*np.add(slm_phase,-1*delta_j_m)),axis=-1),axis=-1))
-
-# this is now exp(1j*delta_j_m)*Vm/|Vm|, to be summed over m
-phaseToSum = np.add(np.array([slm_phase]*npx*npy),Vm_phase[m])
-
-# now we sum exp(1j*delta_j_m)*Vm/|Vm| over m and find the argument
-slm_phase = np.angle(np.sum(np.exp(1j*phaseToSum),axis=0))
-
-# create slm field
+# create final SLM field
 slm = np.exp(1j*slm_phase)
 
+# take the Fourier Transform to get the signal
 ft = fft.fft2((slm))
 
-print(perf(xm,ym,abs(ft)))
+print("GS result after ",i," iterations: e , u, sigma = ",perf(xm,ym,abs(ft)))
 
 plt.imshow(fft.fftshift(abs(ft)))
 plt.title('Result')
