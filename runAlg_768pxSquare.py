6e7352b2a3d98f89dd56461489afcd52a732dd1a
frankelm <maxnfrankel@gmail.com>
2022-11-18 11:33:49 +0100

    Created separate scripts for testing algorithm on 768x768 array and SLM

--- runAlg_768pxSquare.py
+++ runAlg_768pxSquare.py
@@ -16,17 +16,17 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 # set the filename you want to save to. Will end in "Holo.bmp"
-name = 'PointArrayGSW'
+name = 'PointArray'
 
 # set the SLM dimensions
-Nx = 1280
-Ny = 1024
+Nx = 768
+Ny = 768
 
 # set the point array dimensions
-npx = 60 # number of pts in x direction
-npy = 60 # number of pts in y direction
-px = 15 # x periodicity
-py = 15 # y periodicity
+npx = 10 # number of pts in x direction
+npy = 10 # number of pts in y direction
+px = 70 # x periodicity
+py = 70 # y periodicity
 
 # form pt array
 xm,ym = PtArrayCoords(npx,npy,px,py)
@@ -44,14 +44,12 @@ initial_slm_phase, perf_SR = SR(Nx,Ny,xm,ym,showGraph=False)
 
 print(perf_SR)
 
-#slm_phase, perf_GS = GS(initial_slm_phase,Nx,Ny,xm,ym,niter=30,showGraph=False) # niter is the number of iterations for the GS algorithm
-#slm_phase, perf_GAA = GAA(initial_slm_phase,Nx,Ny,xm,ym,niter=30,xi=0.8,showGraph=False) # xi is a number between 0 and 1 that determines how much the maximization of Sum(log|V_m|) is prioritized over Sum(|Vm|)
+slm_phase, perf_GS = GS(initial_slm_phase,Nx,Ny,xm,ym,niter=30,showGraph=False) # niter is the number of iterations for the GS algorithm
+#slm_phase, perf_GAA = GAA(initial_slm_phase,Nx,Ny,xm,ym,niter=30,xi=0.74,showGraph=False) # xi is a number between 0 and 1 that determines how much the maximization of Sum(log|V_m|) is prioritized over Sum(|Vm|)
 #slm_phase, perf_DS = DS(initial_slm_phase,Nx,Ny,xm,ym,niter=int(round(Nx*Ny*1.3)),f=0.5,showGraph=True)
+#slm_phase, perf_GSW = GSW(initial_slm_phase,Nx,Ny,xm,ym,niter=30,showGraph=False)
 
-
-slm_phase, perf_GSW = GSW(initial_slm_phase,Nx,Ny,xm,ym,niter=500,showGraph=False)
-
-print(perf_GSW)
+print(perf_GS)
 
 # uncomment the code below to save the hologram
 # extract slm_phase
@@ -65,4 +63,4 @@ slm_phase = np.round((slm_phase + cmath.pi)/(2*cmath.pi)*255)
 # save hologram
 im = Image.fromarray(slm_phase)
 im = im.convert('RGB')
-im.save(name+"Holo.bmp")
+im.save(name+"GSHolo.bmp")
---
+++ runAlg_ForExperiment.py
@@ -0,0 +1,66 @@
+# this is the script from which the different algorithms can be run
+
+from RMModule import RM
+from SModule import S
+from SRModule import SR
+from GSModule import GS
+from GAAModule import GAA
+#from DSModule import DS
+from GSWModule import GSW
+
+from PtArrayModule import PtArrayCoords
+
+import numpy as np
+import cmath
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# set the filename you want to save to. Will end in "Holo.bmp"
+name = 'PointArray'
+
+# set the SLM dimensions
+Nx = 1024
+Ny = 1280
+
+# set the point array dimensions
+npx = 60 # number of pts in x direction
+npy = 60 # number of pts in y direction
+px = 15 # x periodicity
+py = 15 # y periodicity
+
+# form pt array
+xm,ym = PtArrayCoords(npx,npy,px,py)
+
+# all the functions (RM, S, SR, GS, GAA, GSW) that perform the different algorithms take inputs in the form (Nx,Ny,xm,ym)
+# we now have all of the inputs. Just comment out all but the algorithm you want to use
+
+#slm_phase, perf_RM = RM(Nx,Ny,xm,ym,showGraph=True)
+#slm_phase, perf_S = S(Nx,Ny,xm,ym,showGraph=True)
+#slm_phase, perf_SR = SR(Nx,Ny,xm,ym,showGraph=True)
+
+# to do the GS, GAA, DS, GSW,find initial guess for SLM phase through SR algorithm
+print('Performing SR Algorithm initial guess for SLM phase')
+initial_slm_phase, perf_SR = SR(Nx,Ny,xm,ym,showGraph=False)
+
+print(perf_SR)
+
+slm_phase, perf_GS = GS(initial_slm_phase,Nx,Ny,xm,ym,niter=30,showGraph=False) # niter is the number of iterations for the GS algorithm
+#slm_phase, perf_GAA = GAA(initial_slm_phase,Nx,Ny,xm,ym,niter=30,xi=0.74,showGraph=False) # xi is a number between 0 and 1 that determines how much the maximization of Sum(log|V_m|) is prioritized over Sum(|Vm|)
+#slm_phase, perf_DS = DS(initial_slm_phase,Nx,Ny,xm,ym,niter=int(round(Nx*Ny*1.3)),f=0.5,showGraph=True)
+#slm_phase, perf_GSW = GSW(initial_slm_phase,Nx,Ny,xm,ym,niter=30,showGraph=False)
+
+print(perf_GS)
+
+# uncomment the code below to save the hologram
+# extract slm_phase
+plt.imshow(slm_phase)
+plt.colorbar()
+plt.show()
+
+# rescale to values from 0 to 255
+slm_phase = np.round((slm_phase + cmath.pi)/(2*cmath.pi)*255)
+
+# save hologram
+im = Image.fromarray(slm_phase)
+im = im.convert('RGB')
+im.save(name+"GSHolo_30iter.bmp")
